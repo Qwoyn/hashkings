d95f6c843b86b646943f57e82edfc2ba6970576d
Daniel Pittman <dpdanpittman@gmail.com>
2018-11-26 17:24:56 -0800

    hashkings planet core contract

--- Contracts/Version 0.1/hashkings.sol
+++
@@ -1,9 +0,0 @@
-*****************************************
-*			   HashKings				*
-*										*
-*Grow and Sell Cannabis on the Ethereum *
-*         Classic Blockchain			*
-*										*
-*										*
-* - Daniel Pittman    					*
-*****************************************	
